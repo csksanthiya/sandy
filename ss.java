9f6ccf5bb67ffcfe5339d68bf7582a2fd8fbf01d
csksanthiya <30924946+csksanthiya@users.noreply.github.com>
2017-08-12 17:01:00 +0530

    Create ss.java

---
+++ ss.java
@@ -0,0 +1,14 @@
+		
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char name[22];
+	clrscr();
+	printf("Enter Your Name :");
+	scanf("%s", name);
+	printf("Hello, %s", name);
+	printf("\nYou are learning C programming at codescracker.com");
+
+	getch();
+}
